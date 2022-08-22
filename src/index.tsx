a0dccb8d5a45ef2ab51bc5b64f8f3e6eacbce6d3
harvey.c <harvey.c@kakaoenterprise.com>
2022-08-22 13:32:13 +0900

    Test commit & push

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import ReactDOM from "react-dom";
 import { kcContext } from "./KcApp/kcContext";
 import { KcApp } from "./KcApp";
+import "./index.css";
+import { defaultKcProps } from "keycloakify";
 
 if (kcContext === undefined) {
   throw new Error();
 }
 ReactDOM.render(
-    <KcApp kcContext={kcContext} />,
+    <KcApp kcContext={kcContext}
+    {...{
+      ...defaultKcProps,
+      "kcHeaderWrapperClass": "my-class",
+    }} />,
   document.getElementById("root")
 );
